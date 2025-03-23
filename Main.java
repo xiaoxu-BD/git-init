3c1248eb6d08faa94ca59f6ddf8e6fb39e44fb5a
Execute <2481386604@qq.com>
2025-03-23 19:31:49 +0800

    use master update program

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * @className: Main
+ * @author: xiaoxu
+ * @date: 2025/3/23 19:31
+ * @Version: 1.0
+ * @description:
+ */   
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java");
+    }
+}
